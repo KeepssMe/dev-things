10072dd43fca33ce467fdca61a0fd35e01d9a0a5
KeepssMe <KeepssMe@gmail.com>
2026-05-12 03:10:35 +0300

    Add mssql. Update versions

--- docker-projects/local-dev/docker-scripts/storage.all.bat
+++ docker-projects/local-dev/docker-scripts/storage.all.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd ..
-docker-compose --file docker-compose.local-dev.yaml --env-file .env up -d storage.mongodb storage.rabbitmq
+docker-compose --file docker-compose.local-dev.yaml --env-file .env up -d storage.mssql storage.mongodb storage.rabbitmq
 
 pause
